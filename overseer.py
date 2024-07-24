439367f9676d4fee32aa6bcd755790f20a1eaf44
Allan Galarza <allan.galarza@gmail.com>
2024-07-24 09:54:20 -0700

    do not attempt to set thumbnails for text posts

--- overseer.py
+++ overseer.py
@@ -214,7 +214,8 @@ class ModOverseer(commands.Bot):
         else:
             embed.description = entry.post_text
             embed.url = entry.post_url
-            embed.set_thumbnail(url=entry.data.thumbnail)
+            if entry.data.thumbnail != "self":
+                embed.set_thumbnail(url=entry.data.thumbnail)
             embed.set_author(name=f"u/{entry.post_author}", url=RedditClient.get_user_url(entry.post_author))
         embed.colour = COMMENT_COLOR if isinstance(entry, QueueCommentEntry) else LINK_COLOR
         if entry.user_reports:
